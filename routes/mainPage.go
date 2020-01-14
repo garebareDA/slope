f36841e50a7ad0df6ec764625262dc8333cdc6c0
garebare <garedaiki@outlook.jp>
2020-01-15 00:56:51 +0900

    fix

--- routes/mainPage.go
+++ routes/mainPage.go
@@ -2,10 +2,9 @@ package routes
 
 import(
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 //MainPage メインページの表示
 func MainPage(c *gin.Context) {
-	c.HTML(http.StatusFound, "index.html", gin.H{})
+	c.HTML(200, "index.html", gin.H{})
 }
